52c6078cc9ea569bbc67893e4d4228b1ad0a709b
Daniel <daniel.araujo.dev@hotmail.com>
2024-03-18 21:50:17 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIOModuloAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("(ObterDataHoraAtual)")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+
+            return Ok(obj);
+        }
+
+        [HttpGet("Aprentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá {nome}, seja bem vindo!";
+            return Ok(new { mensagem });
+        }
+       
+    }
+}
